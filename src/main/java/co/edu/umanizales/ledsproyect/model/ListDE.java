3c398c16e181e3ea961d5c9ebbc5b987a1093ffd
marting3112 <mgonzalez100247@umanizales.edu.co>
2023-05-11 14:09:57 -0500

    Se realiza el algoritmo del método de encender los bombillos del centro a sus extremos.

--- src/main/java/co/edu/umanizales/ledsproyect/model/ListDE.java
+++ src/main/java/co/edu/umanizales/ledsproyect/model/ListDE.java
@@ -16,6 +16,7 @@ public class ListDE {
     private int size;
     private List<Led> leds = new ArrayList<>();
 
+
     public void addLed(Led led) {
         if (head == null) {
             head = new NodeDE(led);
@@ -77,6 +78,42 @@ public class ListDE {
         }
         size++;
     }
+
+    /*
+    Verifico si la cabeza (head) no es nula.
+Si la cabeza no es nula, procedo con los siguientes pasos. De lo contrario, salgo del método.
+Inicializo una variable temporal (temp) con el valor de la cabeza.
+Establezco la variable "pasos" en 1 para llevar un registro del número de pasos.
+Declaro una variable "medium" para almacenar el valor medio.
+Si el tamaño de la lista es impar (size % 2 != 0):
+Calculo el valor medio sumando 1 al resultado de la división entera de size entre 2.
+Inicio un bucle mientras el nodo temporal (temp) no sea nulo:
+Verifico si el número de pasos es igual al valor medio:
+Creo una variable temporal (tempNext) para almacenar el siguiente nodo.
+Establezco el estado del Led en el nodo temporal actual (temp) como encendido (true).
+Establezco la fecha de encendido del Led en el nodo temporal actual (temp) con la hora actual.
+Inicio un bucle mientras el siguiente nodo temporal (tempNext) no sea nulo:
+Espero durante 1 segundo.
+Establezco el estado del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) como apagados (false).
+Establezco la fecha de apagado del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) con la hora actual.
+Actualizo el nodo temporal actual (temp) al nodo anterior (prev).
+Actualizo el nodo temporal siguiente (tempNext) al nodo siguiente (next).
+Establezco el estado del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) como encendidos (true).
+Establezco la fecha de encendido del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) con la hora actual.
+Incremento el número de pasos.
+Actualizo el nodo temporal (temp) al siguiente nodo.
+Si el tamaño de la lista es par (size % 2 == 0):
+Calculo el valor medio dividiendo el tamaño de la lista entre 2.
+Inicio un bucle mientras el nodo temporal (temp) no sea nulo:
+Verifico si el número de pasos es igual al valor medio:
+Creo una variable temporal (tempNext) para almacenar el siguiente nodo.
+Establezco el estado del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) como encendidos (true).
+Establezco la fecha de encendido del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) con la hora actual.
+Inicio un bucle mientras el siguiente nodo temporal (tempNext) no sea nulo:
+Espero durante 1 segundo.
+Establezco el estado del Led en el nodo temporal actual (temp) y en el nodo temporal siguiente (tempNext) como apagados (false).
+Establezco la fecha de apagado del Led en el nodo temporal actual (temp) y en el nodo temporal
+     */
 
     public  void turnOnLight(){
         if (head != null) {
